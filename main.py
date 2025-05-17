60d3eb08e5c56471fc7e5901d88e769290f89a0d
kimjunhyeong <cosyq1305@naver.com>
2025-05-18 00:08:57 +0900

    3

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from google import genai
+
 app = FastAPI()
+
+client = genai.Client(api_key="YOUR_API_KEY")
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
+
+@app.get("/test")
+def test():
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+    )
+    return {"response": response.text}
